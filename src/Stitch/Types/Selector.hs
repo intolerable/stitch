5f8da1e65aa23961e74897b32fb979337e080f43
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-09 16:31:05 +0100

    add support for the & operator

--- src/Stitch/Types/Selector.hs
+++ src/Stitch/Types/Selector.hs
@@ -3,6 +3,7 @@ module Stitch.Types.Selector where
 import Data.Text (Text)
 import Data.Monoid
 import Data.String
+import qualified Data.Text as Text
 
 -- | Represents a CSS selector. Can be combined with other 'Selector's using its 'Monoid' instance.
 newtype Selector = Selector Text
@@ -15,4 +16,9 @@ instance Monoid Selector where
   mempty = ""
   Selector "" `mappend` Selector y = Selector y
   Selector x `mappend` Selector "" = Selector x
-  Selector x `mappend` Selector y = Selector (x <> " " <> y)
+  Selector x `mappend` Selector y =
+    if "&" `Text.isInfixOf` y
+      then
+        Selector (Text.replace "&" x y)
+      else
+        Selector (x <> " " <> y)

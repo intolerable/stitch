59520fb36590524ca6364cd750527f4e56393d54
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-09 21:14:26 +0100

    space out the tests

--- test/StitchSpec.hs
+++ test/StitchSpec.hs
@@ -13,41 +13,51 @@ main = hspec spec
 spec :: Spec
 spec = do
 
-  match "empty.css" $ return ()
+  match "empty.css" $
+    return ()
+
   match "basic_import.css" $
     cssImport "empty.css"
+
   match "basic_props.css" $
     "body" ?
       "color" .= "red"
+
   match "multiple_props.css" $
     "body" ? do
       "color" .= "red"
       "background-color" .= "blue"
+
   match "basic_nested_props.css" $
     "body" ? do
       "color" .= "red"
       "h1" ?
         "font-size" .= "200%"
+
   match "basic_ampersand.css" $
     "a" ? do
       "color" .= "red"
       "&:hover" ?
         "color" .= "blue"
+
   match "ampersand_after.css" $ do
     ".button" ?
       "h1" ?
         ".selected &" ?
           "color" .= "red"
+
   match "multiple_ampersands.css" $
     "a, h1" ? do
       "color" .= "red"
       "&:hover" ?
         "color" .= "blue"
+
   match "basic_prefix.css" $
     "body" ?
       "font" -: do
         "size" .= "1.5em"
         "family" .= "Helvetica, sans"
+
   match "multiple_selectors.css" $
     "h1, h2" ?
       "a" ? do

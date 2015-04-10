3ef009e2ca25f30ac7c19a1658ab7f05037fe755
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-10 17:18:18 +0100

    basic benchmarks

---
+++ bench/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Stitch
+import Stitch.Render
+
+import Control.Monad
+import Criterion.Main
+import Data.Monoid
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "css"
+    [ bench "basic" $ nf renderCSS example
+    , bench "compressed" $ nf (renderCSSWith compressed) example ] ]
+
+example :: CSS
+example =
+  forM_ [1..1000] $ \n -> do
+    mconcat (replicate n "x") ?
+      "color" .= "green"

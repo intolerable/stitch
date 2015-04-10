42e9efea50f01ebb580548811be1d09b7a4d94b4
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-10 19:30:14 +0100

    add a test for the monoid instance finally

---
+++ test/Stitch/Types/SelectorSpec.hs
@@ -0,0 +1,31 @@
+module Stitch.Types.SelectorSpec where
+
+import Stitch.Types.Selector
+
+import Data.Monoid
+import Test.Hspec hiding (Selector)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+  describe "Selector" $ do
+    describe "Monoid" $ do
+      it "should hold that x <> mempty == x" $ do
+        let smempty = mempty :: Selector
+        "h1" <> smempty `shouldBe` "h1"
+        ("h1" <> "h2") <> smempty `shouldBe` "h1 h2"
+        "&:hover" <> smempty `shouldBe` "&:hover"
+      it "should hold that mempty <> x == x" $ do
+        let smempty = mempty :: Selector
+        smempty <> "h1" `shouldBe` "h1"
+        smempty <> ("h1" <> "h2") `shouldBe` "h1 h2"
+        smempty <> "&:hover" `shouldBe` "&:hover"
+      it "should hold that x <> (y <> z) == (x <> y) <> z" $ do
+        let test x y z = (x <> (y <> z) :: Selector) `shouldBe` ((x <> y) <> z)
+        test "h1" mempty "&:hover"
+        test "h1" "h2" "h3"
+        test mempty mempty mempty
+        test "&:active" "&:hover" ".red &"

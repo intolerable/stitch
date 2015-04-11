155b905d205faea402341af2aa7f4614483864d8
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-11 14:50:36 +0100

    add the missing monadtrans instance

--- src/Control/Monad/Trans/Stitch.hs
+++ src/Control/Monad/Trans/Stitch.hs
@@ -6,11 +6,12 @@ import Stitch.Types
 
 import Control.Applicative
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Writer.Strict
 import Data.Monoid
 
 newtype StitchT m a = StitchT (WriterT Block m a)
-  deriving (Functor, Applicative, Monad, MonadIO)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadTrans)
 
 instance (Applicative m, Monoid a) => Monoid (StitchT m a) where
   mempty = pure mempty
